ce19ef25293d326482e2bee8ea4a1181996cec28
AnandTiwary20 <anandtiwari0019@gmail.com>
2025-11-02 22:16:13 +0530

    Initial Ui creation and working check

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-function App(){
-    let saveTodoList = (event)=>{
-
-
-        let toname=event.target.toName.value;
-        alert(toname);
-        event.preventDefault();
-
-    }
-    return (
-        <div className = "App">
-            <h1>ToDO List </h1>
-            <form onSubmit = {saveTodoList}>
-                <input type="text"  name = 'toName' placeholder="Enter a task"  />
-                <button type="submit"> Save</button>
-            </form>
-            
-        </div>
-
-)}
+  return (
+    <div className="app">
+      <h1>My To-Do List</h1>
+      <form className="todo-form">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Add a new task..."
+          className="todo-input"
+        />
+        <button type="submit" className="add-button">
+          Add Task
+        </button>
+      </form>
+      
+      <ul className="todo-list">
+        {/* Todo items will be rendered here */}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
