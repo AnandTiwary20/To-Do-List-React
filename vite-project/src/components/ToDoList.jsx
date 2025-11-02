1aee61daaec01b808b1fef2c7857b9f313fb6398
AnandTiwary20 <anandtiwari0019@gmail.com>
2025-11-02 22:43:38 +0530

    Added error handling functions and improved the Css

--- vite-project/src/components/ToDoList.jsx
+++ vite-project/src/components/ToDoList.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import ToDoItem from './ToDoItem';
 
 const ToDoList = ({ todos, onToggleComplete, onDelete }) => {
+
+  if(todos.length ==0){
+    return<p className ="Empty - Message Please Add A task "> No Task Yet</p>
+  }
   return (
     <ul className="todo-list">
-      {todos.length === 0 ? (
-        <li className="empty-state">No tasks to display. Add a new task above!</li>
-      ) : (
-        todos.map(todo => (
+   {todos.map((todo , index) => (
+       
           <ToDoItem
             key={todo.id}
             todo={todo}
             onToggleComplete={onToggleComplete}
             onDelete={onDelete}
           />
-        ))
+        )
       )}
     </ul>
   );
